d0dac3e28c242adef83d6c06d6f9ae3d6d558603
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-04-04 22:53:26 -0500

    Update color of gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,8 +72,7 @@ export default {
       },
       backgroundImage: {
         "radial-gradient": "radial-gradient(var(--tw-gradient-stops))",
-        "conic-gradient":
-          "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
+        "conic-gradient": "conic-gradient(from 225deg, #AC0C04, #AC0C04)",
         "benefit-card-1": "url(assets/benefits/card-1.svg)",
         "benefit-card-2": "url(assets/benefits/card-2.svg)",
         "benefit-card-3": "url(assets/benefits/card-3.svg)",
