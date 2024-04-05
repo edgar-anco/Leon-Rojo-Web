2f8d0f37e445885df5b8b1ea548822b697b92848
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-04-04 19:13:15 -0500

    Update background

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,7 +19,7 @@ import disc02 from "./disc-02.svg";
 import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
-import background from "./background.jpg";
+import background from "./background.png";
 
 import curve from "./principal/curve.png";
 import robot from "./principal/robot.jpg";
